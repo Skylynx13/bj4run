2a50dd0314b18032b17826c40255482871f69da7
qxu <qxu@travelsky.com>
2016-06-21 11:21:03 +0800

    Init. Import record reader.

---
+++ src/com/qxu/bj4run/record/RecordReader.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2016,TravelSky. 
+ * All Rights Reserved.
+ * TravelSky CONFIDENTIAL
+ * 
+ * Project Name:bj4run
+ * Package Name:com.qxu.bj4run.record
+ * File Name:RecordReader.java
+ * Date:2016-6-21 上午11:19:50
+ * 
+ */
+package com.qxu.bj4run.record;
+
+ /**
+ * ClassName: RecordReader <br/>
+ * Description: TODO <br/>
+ * Date: 2016-6-21 上午11:19:50 <br/>
+ * <br/>
+ * 
+ * @author qxu(邮箱)
+ * 
+ * 修改记录
+ * @version 产品版本信息 yyyy-mm-dd 姓名(邮箱) 修改信息<br/>
+ * 
+ */
+
+public class RecordReader {
+
+    public static void main (String[] args) {
+        System.out.println("Record Reader Start...");
+        System.out.println("Record Reader End.");
+    }
+}
